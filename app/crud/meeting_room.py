0529077c4faec24b2aa8333bdef00f39c638ae07
Alexander Markov <markoff.dev@ya.ru>
2023-02-26 18:52:12 +0200

    add CRUDBase, refactoring meeting room, add schemas for reservation

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,80 +1,26 @@
 from typing import Optional
 
-from fastapi.encoders import jsonable_encoder
-from sqlalchemy import exc, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.exceptions import CustomException
+from app.crud.base import CRUDBase
 from app.models.meeting_room import MeetingRoom
-from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomUpdate
 
 
-async def create_meeting_room(
-        new_room: MeetingRoomCreate,
-        session: AsyncSession,
-) -> MeetingRoom:
-    new_room_data = new_room.dict()
-    db_room = MeetingRoom(**new_room_data)
-    session.add(db_room)
+class CRUDMeetingRoom(CRUDBase):
 
-    try:
-        await session.commit()
-    except exc.IntegrityError as ex:
-        if 'unique' in str(ex.orig).lower():
-            raise CustomException(
-                422,
-                'Переговорка с таким именем уже существует!',
+    async def get_room_id_by_name(
+            self,
+            room_name: str,
+            session: AsyncSession,
+    ) -> Optional[int]:
+        db_room_id = await session.execute(
+            select(MeetingRoom.id).where(
+                MeetingRoom.name == room_name
             )
-
-    await session.refresh(db_room)
-
-    return db_room
-
-
-async def read_all_rooms_from_db(session: AsyncSession) -> list[MeetingRoom]:
-    db_rooms = await session.scalars(select(MeetingRoom))
-    return db_rooms.all()
+        )
+        db_room_id = db_room_id.scalars().first()
+        return db_room_id
 
 
-async def get_meeting_room_by_id(
-        room_id: int,
-        session: AsyncSession,
-) -> Optional[MeetingRoom]:
-    db_room = await session.get(MeetingRoom, room_id)
-    return db_room
-
-
-async def update_meeting_room(
-        db_room: MeetingRoom,
-        room_in: MeetingRoomUpdate,
-        session: AsyncSession,
-) -> MeetingRoom:
-    obj_data = jsonable_encoder(db_room)
-    update_data = room_in.dict(exclude_unset=True)
-
-    for field in obj_data:
-        if field in update_data:
-            setattr(db_room, field, update_data[field])
-
-    session.add(db_room)
-
-    try:
-        await session.commit()
-    except exc.IntegrityError as ex:
-        if 'unique' in str(ex.orig).lower():
-            raise CustomException(
-                422,
-                'Переговорка с таким именем уже существует!',
-            )
-
-    await session.refresh(db_room)
-    return db_room
-
-
-async def delete_meeting_room(
-        db_room: MeetingRoom,
-        session: AsyncSession,
-) -> MeetingRoom:
-    await session.delete(db_room)
-    await session.commit()
-    return db_room
+meeting_room_crud = CRUDMeetingRoom(MeetingRoom)
---
+++ app/schemas/reservation.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from pydantic import root_validator, validator
+
+
+def ReservationBase(BaseModel):
+    from_reserve: datetime
+    to_reserve: datetime
+
+
+def ReservationUpdate(ReservationBase):
+    @validator('from_reserve')
+    def check_from_reserve_later_than_now(cls, value):
+        if value <= datetime.now():
+            raise ValueError(
+                'Время начала бронирования '
+                'не может быть меньше текущего времени'
+            )
+        return value
+
+    @root_validator(skip_on_failure=True)
+    def check_from_reserve_before_to_reserve(cls, values):
+        if values['from_reserve'] >= values['to_reserve']:
+            raise ValueError(
+                'Время начала бронирования '
+                'не может быть больше времени окончания'
+            )
+        return values
+
+
+def ReservationCreate(ReservationUpdate):
+    meetingroom_id: int
+
+
+class ReservationDB(ReservationBase):
+    id: int
+    meetingroom_id: int
+
+    class Config:
+        orm_mode = True
